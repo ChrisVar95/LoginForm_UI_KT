4944819b0bb68ae5f1f7baee3becf2111396911c
ChrisVar95 <christina.vargka@gmail.com>
2023-01-24 18:23:49 +0200

    Login form UI

---
+++ app/src/main/java/com/example/loginform/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.loginform
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.loginform.ui.theme.LoginFormTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginFormTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    loginForm()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun loginForm() {
+    var usernameInput: String by remember { mutableStateOf("") }
+    var passwordInput: String by remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Login",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 16.dp)
+        )
+        OutlinedTextField(
+            value = usernameInput,
+            onValueChange = {usernameInput = it},
+            singleLine = true,
+            label = { Text(text = "Username") },
+            trailingIcon = { Icon(imageVector = Icons.Default.Email, contentDescription = "Email Icon")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+        OutlinedTextField(
+            value = passwordInput,
+            onValueChange = {passwordInput = it},
+            singleLine = true,
+            label = { Text(text = "Password") },
+            trailingIcon = { Icon(imageVector = Icons.Default.Lock, contentDescription = "Password")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(72.dp)
+                .padding(vertical = 8.dp)
+        )
+        {
+            Text(text = "Submit", fontSize = 20.sp)
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginFormTheme {
+        loginForm()
+    }
+}
